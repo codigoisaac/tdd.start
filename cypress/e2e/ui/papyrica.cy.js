ac7c71a9586091920a159df070728f680411127c
IsaacMuniz <isaacmuniz@protonmail.com>
2023-09-19 03:44:17 -0300

    My first cypress test :)

---
+++ cypress/e2e/ui/papyrica.cy.js
@@ -0,0 +1,11 @@
+describe("Papyrica App", () => {
+  it("visits the app home", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('div[data-test="book-list"]').should("exist");
+    cy.get("div.book-item").should((books) => {
+      expect(books).to.have.length(2);
+      const titles = [...books].map((b) => b.querySelector("h2").innerHTML);
+      expect(titles).to.deep.equal(["Refactoring", "Domain-Driven Design"]);
+    });
+  });
+});
--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import { Typography } from "@mui/material";
 function App() {
   return (
     <div className="App">
-      <Typography variant="h1" component="h1" data-test="siteTitle">
+      <Typography variant="h1" component="h1">
         Papyrica
       </Typography>
+
+      <div data-test="book-list">
+        <div className="book-item">
+          <h2 className="book-title">Refactoring</h2>
+        </div>
+
+        <div className="book-item">
+          <h2 className="book-title">Domain-Driven Design</h2>
+        </div>
+      </div>
     </div>
   );
 }
