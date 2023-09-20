59126a36745aca65622c5821920406e113407644
IsaacMuniz <isaacmuniz@protonmail.com>
2023-09-19 21:50:35 -0300

    Test db interaction

--- cypress/e2e/ui/papyric.cy.js
+++ cypress/e2e/ui/papyric.cy.js
@@ -1,11 +1,46 @@
+import { api } from "../../../src/services/api";
+
 describe("Papyric App", () => {
-  it("visits the app home", () => {
+  before(() => {
+    // - empty the book list
+    return api.delete("books?_cleanup=true").catch((error) => error);
+  });
+
+  beforeEach(() => {
+    // - add books
+    const books = [
+      { name: "Refactoring", id: 1 },
+      { name: "Domain-Driven Design", id: 2 },
+      { name: "Building Microsservices", id: 3 }
+    ];
+
+    return books.map((book) =>
+      api.post("books", book, {
+        headers: { "Content-Type": "application/json" }
+      })
+    );
+  });
+
+  afterEach(() => {
+    // - empty the book list
+    return api.delete("books?_cleanup=true").catch((error) => error);
+  });
+
+  it("Shows the book list", () => {
     cy.visit("http://localhost:3000/");
+
     cy.get('div[data-test="book-list"]').should("exist");
+
     cy.get("div.book-item").should((books) => {
-      expect(books).to.have.length(2);
-      const titles = [...books].map((b) => b.querySelector("h2").innerHTML);
-      expect(titles).to.deep.equal(["Refactoring", "Domain-Driven Design"]);
+      expect(books).to.have.length(3);
+
+      const titles = [...books].map(
+        (book) => book.querySelector("h2").innerHTML
+      );
+
+      expect(titles).to.contain("Refactoring");
+      expect(titles).to.contain("Domain-Driven Design");
+      expect(titles).to.contain("Building Microsservices");
     });
   });
 });
